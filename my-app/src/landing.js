ffe2d146cd85e85084d3703be7f12f42214d0f59
Yogakani <yogakani32@gmail.com>
2024-03-22 15:58:26 +0530

    landing design changes

--- my-app/src/landing.js
+++ my-app/src/landing.js
@@ -3,29 +3,30 @@ import Login from './login';
 export default function Landing() {
     
     return(
-       <div className="card position-absolute p-3 top-50 start-50 translate-middle" style={{width:1000}}>
-            <div className="container">
-                <div className="row">
-                    <div className="col-6">
-                        <Login></Login>
-                    </div>
-                    <div className="col-6">
-                        <div className="card border border-0" style={{width:300}}>
-                            <div className="card-body">
-                                <div className="row mb-5"></div>
-                                <div className="row mb-5"></div>
-                                <div className="row mb-5"></div>
-                                <div className="row mb-5"></div>
-                                <div className="row">
-                                    <div className="col">
-                                        <img src="https://cdn.dribbble.com/userupload/4283394/file/original-1956ecc77f662d432fe185f9668ee218.png?resize=250x" alt="CC-Img" className="rounded" width={450}></img>
-                                    </div>
+        <>
+            <div className="card position-absolute p-3 top-50 start-50 translate-middle mt-5" style={{width:1000, backgroundColor:'#000435'}}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6">
+                            <Login></Login>
+                        </div>
+                        <div className="col-6">
+                            <div className="row mb-3"></div>
+                            <div className="row mb-3">
+                                <div className="col">
+                                    <img src={process.env.PUBLIC_URL + '/landing-img.jpg'} alt="mobile-img" className="rounded" width={450}></img>
+                                </div>
+                            </div>
+                            <div className='row mb-3'></div>
+                            <div className='row mb-5'>
+                                <div className='col'>
+                                    <img src={process.env.PUBLIC_URL + '/landing2.jpg'} alt='laptop-img' className='rounded' width={450}></img>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-       </div>
+        </>
     );
 }
